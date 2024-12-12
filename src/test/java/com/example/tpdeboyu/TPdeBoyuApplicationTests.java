1e3892a24e7d544b942cc11cb4d265442bbe9946
BoyuZHANG <boyuzhang333@gmail.com>
2024-12-12 13:55:56 +0100

    initialiser

---
+++ src/test/java/com/example/tpdeboyu/TPdeBoyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpdeboyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TPdeBoyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
