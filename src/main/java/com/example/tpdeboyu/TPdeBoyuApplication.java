361d736bdeea59c4fef02f43be69c8430bc7da1f
BoyuZHANG <boyuzhang333@gmail.com>
2024-12-12 13:55:56 +0100

    initialiser

---
+++ src/main/java/com/example/tpdeboyu/TPdeBoyuApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpdeboyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TPdeBoyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TPdeBoyuApplication.class, args);
+	}
+
+}
